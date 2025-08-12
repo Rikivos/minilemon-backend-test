2c646984a840e91e9660c1d992e4af40e3e72160
Rikivos <rizkypenpenz@gmail.com>
2025-08-13 03:29:26 +0700

    refactor : change README.md

--- src/infrastructure/swagger/swaggerConfig.js
+++ src/infrastructure/swagger/swaggerConfig.js
@@ -1,34 +1,27 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const isProd = process.env.NODE_ENV === "production";
-
-const routesPath = isProd
-  ? path.resolve(__dirname, "../../interface/routes/*.js") 
-  : path.resolve(__dirname, "../../interface/routes/*.js"); 
-
-console.log("Swagger reading API docs from:", routesPath);
 
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "My API",
+      title: " Api test backend developer in Minilemon",
       version: "1.0.0",
       description: "API Documentation",
     },
     servers: [
       {
-        url: process.env.SWAGGER_SERVER_URL || "https://minilemon-backend-test.vercel.app/api",
+        url: "https://minilemon-backend-test.vercel.app/api",
+        description: "Development Server",
       },
     ],
   },
-  apis: [routesPath],
+  apis: [path.resolve(__dirname, "../../interface/routes/*.js")],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
